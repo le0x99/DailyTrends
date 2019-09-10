277a10fe5d635e44425de68eb9966c096c6ca110
le0x99 <36996220+le0x99@users.noreply.github.com>
2019-09-10 21:30:57 +0200

    Update collect.py

--- DailyTrends/collect.py
+++ DailyTrends/collect.py
@@ -6,9 +6,9 @@ from DailyTrends.rescale import qAggr
 
 def collect_data(q:str,
                  save:bool=False,
-                dest:str=None) -> pd.DataFrame:
+                dest:str=None, verbose:bool=False) -> pd.DataFrame:
   #gets the unscaled data pieces
   frames = collect_frames(q)
   #"quick-aggregates" the data pieces and rescales the values using overlaps
-  df = qAggr(frames)
+  df = qAggr(frames, verbose=verbose)
   return df if not save else df.to_csv(dest)
