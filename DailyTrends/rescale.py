94390b3bf37a0d8d982440a3c14af253924f693e
le0x99 <36996220+le0x99@users.noreply.github.com>
2019-09-22 18:07:01 +0200

    catch exceptions using a recursive qAggr

--- DailyTrends/rescale.py
+++ DailyTrends/rescale.py
@@ -22,7 +22,10 @@ def aggr(x1:pd.DataFrame,
 def qAggr(frames:list, verbose:bool=False) -> pd.DataFrame:
     """convienent function to quick-aggregate n data pieces"""
     """IMPORTANT : input needs to be ordered where t(args[0]) < t(args[1]) < ... < t(args[-1])"""
-    df = aggr(frames[0], frames[1], verbose=verbose)
-    for x in frames[2:]:
-        df = aggr(df, x, verbose=verbose)
-    return df.asfreq("D")
+    try:
+        df = aggr(frames[0], frames[1], verbose=verbose)
+        for x in frames[2:]:
+            df = aggr(df, x, verbose=verbose)
+        return df.asfreq("D")
+    except:
+        return qAggr(frames[1:], verbose=verbose)
