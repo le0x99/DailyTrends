71da639421c8393326763d8f3f54e2c8b7b37617
le0x99 <36996220+le0x99@users.noreply.github.com>
2019-09-22 19:35:31 +0200

    Update collect.py

--- DailyTrends/collect.py
+++ DailyTrends/collect.py
@@ -1,23 +1,16 @@
 import pandas as pd
 import numpy as np
-from functools import reduce
 
 from DailyTrends.request import collect_frames
 from DailyTrends.rescale import qAggr
 
-def collect_data(q:str,
+def collect_data(q:None,
                  save:bool=False,
                 dest:str=None, verbose:bool=False) -> pd.DataFrame:
-  if type(q) == str:
+    if type(q) != str and type(q) != list:
+        raise TypeError("Use string for single query search / list of strings for multiple keywoards.")
     #gets the unscaled data pieces
     frames = collect_frames(q)
     #"quick-aggregates" the data pieces and rescales the values using overlaps
     df = qAggr(frames, verbose=verbose)
     return df if not save else df.to_csv(dest)
-  elif type(q) == list:
-    return reduce(lambda left,right: pd.merge(left,right,left_index=True, right_index=True),
-                  [collect_data(i, save, dest) for i in q])
-  else:
-    raise TypeError("Use string for single query search / list of strings for multiple keywoards.")
-    
-    
