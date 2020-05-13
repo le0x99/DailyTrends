332c360aafbb53b309930ed4e4a033488ee769ef
Leonard D. Vorbeck <36996220+le0x99@users.noreply.github.com>
2020-05-13 15:47:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ setup(
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.6"
-        "Programming Language :: Python :: 3.5"
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.5",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
 )
