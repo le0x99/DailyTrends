f0a96fe5ced9dd4d9a05f002ce7b9856356df991
Leonard D. Vorbeck <36996220+le0x99@users.noreply.github.com>
2020-05-13 15:29:54 +0200

    major update 2

--- DailyTrends/rescale.py
+++ DailyTrends/rescale.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np   
 
 def aggr(x1:pd.DataFrame,
-         x2:pd.DataFrame, verbose:bool=False) -> pd.DataFrame:
+         x2:pd.DataFrame, verbose:bool=True) -> pd.DataFrame:
     """ Input 2 dataframes with exactly 1 column, which is indexed."""
     """ Note : t(x1) << t(x2)"""
     #Find common
     common = pd.merge(x1, x2, left_index=True, right_index=True)
     print("Potential Ratio Points : ", len(common)) if verbose else None
     #Drop Inf and 0
-    common = common.replace([np.inf, -np.inf, 0], np.nan).dropna()
+    common = common.replace([np.inf, -np.inf, 0, .9999], np.nan).dropna()
     print("Usable Ratio Points : ", len(common)) if verbose else None
     if len(common) < 1:
         raise ValueError("Ratio could not be calculated, try bigger overlap.")
@@ -23,18 +23,21 @@ def aggr(x1:pd.DataFrame,
     df = x1.append(x2.drop(pd.merge(x1, x2, left_index=True, right_index=True).index)*ratio)
     return df/max(df[df.columns[0]])*100 if ratio > 1 else df
 
-def qAggr(frames:list, verbose:bool=False) -> pd.DataFrame:
+def qAggr(frames:list, verbose:bool=True) -> pd.DataFrame:
     """convienent function to quick-aggregate n data pieces"""
     """IMPORTANT : input needs to be ordered where t(args[0]) < t(args[1]) < ... < t(args[-1])"""
-    if len(frames) == 0:
-         raise Exception("There is no data for this particular query")
+    if len(frames) == 0:     
+        raise ValueError("There is no data for this particular query")
     elif len(frames) == 1:
-         return frames[0]
-    else:            
-             try:
-                 df = aggr(frames[0], frames[1], verbose=verbose)
-                 for x in frames[2:]:
-                     df = aggr(df, x, verbose=verbose)
-                 return df.asfreq("D")
-             except:
-                 return qAggr(frames[1:], verbose=verbose)
+        return frames[0]
+    else:
+        try:
+            df = aggr(frames[0], frames[1], verbose=verbose)
+            for x in frames[2:]:
+                
+                df = aggr(df, x, verbose=verbose)
+            return df.asfreq("D")
+        
+        except:
+            
+            return qAggr(frames[1:], verbose=verbose)
