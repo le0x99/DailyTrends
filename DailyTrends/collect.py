9f3580cb6a3bbc6da04c79633f400d28bf4cf992
Leonard D. Vorbeck <36996220+le0x99@users.noreply.github.com>
2020-05-13 15:30:15 +0200

    Update collect.py

--- DailyTrends/collect.py
+++ DailyTrends/collect.py
@@ -4,13 +4,13 @@ import numpy as np
 from DailyTrends.request import collect_frames
 from DailyTrends.rescale import qAggr
 
-def collect_data(q:None,
+def collect_data(q:None,start:str="2004-01-01", end:str="TODAY", geo="",
                  save:bool=False,
                 dest:str=None, verbose:bool=False) -> pd.DataFrame:
     if type(q) != str and type(q) != list:
         raise TypeError("Use string for single query search / list of strings for multiple keywoards.")
     #gets the unscaled data pieces
-    frames = collect_frames(q)
+    frames = collect_frames(q, start, end, geo)
     #"quick-aggregates" the data pieces and rescales the values using overlaps
     df = qAggr(frames, verbose=verbose)
     return df if not save else df.to_csv(dest)
